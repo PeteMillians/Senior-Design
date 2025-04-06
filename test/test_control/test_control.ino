fa176ed9d788f30a3d31bd3913f5e9f8c4f17c23
PeteMillians <pete@millians.com>
2025-04-06 17:25:46 -0400

    Update test script for new stall logic

--- test/test_control/test_control.ino
+++ test/test_control/test_control.ino
@@ -3,7 +3,7 @@
 /* Constants declared in header */
 const int CURRENT_PINS[5] = {A1, A2, A3, A4, A5};
 const int MOTOR_PINS[5] = {3, 5, 6, 9, 11};
-const float CURRENT_THRESHOLD = 204.6;   // Example current threshold 
+const float CURRENT_THRESHOLD = 450;   // Example current threshold 
 const float SIGNAL_THRESHOLD = 6.1;   // Example voltage threshold
 const Servo MOTORS[5];
 float totalRotation[5] = {0.0, 0.0, 0.0, 0.0, 0.0}; // array of total angle rotated by each motor
@@ -27,18 +27,20 @@ void loop() {
 
   float filteredSignal = 150;   // On signal for turn state
 
-  if(totalTime > 500) {
-    filteredSignal = 2;   // Off signal for release stae
-  }
+//   if(totalTime > 500) {
+//     filteredSignal = 2;   // Off signal for release stae
+//   }
 
-  Serial.print("Filtered Signal = ");
-  Serial.println(filteredSignal);
+//   Serial.print("Filtered Signal = ");
+//   Serial.println(filteredSignal);
 
   float currentReadings[5];
   
   for (int i = 0; i < 5; i++) {
     currentReadings[i] = float(analogRead(CURRENT_PINS[i]));
+    Serial.print(currentReadings[i] + String(" "));
     }
+    Serial.println();
 
   ControlMotors(filteredSignal, currentReadings);
 
@@ -56,6 +58,7 @@ void ControlMotors(float filteredSignal, float sensorReadings[]) {
         - sensorReadings (floats): Current sensor readings
     */
 
+    int stallIndex = -1;    // Initialize stallIndex out of range
 
     // Check that the EMG signal is powering the motors
     if (filteredSignal > SIGNAL_THRESHOLD) {
@@ -63,32 +66,34 @@ void ControlMotors(float filteredSignal, float sensorReadings[]) {
         // Iterate through each current sensor pin
         for (int i = 0; i < 5; i++) {
 
-            if (sensorReadings[i] > CURRENT_THRESHOLD) {    // If overdrawing current
+            if (sensorReadings[i] < CURRENT_THRESHOLD) {    // If overdrawing current
 
-                if (isOverdrawn[i]) {   // If it is consecutively overdrawn
+                if (isOverdrawn[i] && i == stallIndex) {   // If it is consecutively overdrawn
                     // Set to hold state
                     MOTORS[i].write(90);
                     continue;  // This should break out of line 133 loop, but remain in for-loop
                 }
 
+                stallIndex = sensorReadings.index(min(sensorReadings)); // Index of stalled motor
+
                 isOverdrawn[i] = true;  // Record that this motor has overdrawn current
 
                 // Continue rotating
-                Serial.println("Hold State");
+                // Serial.println("Hold State");
                 float rotation = constrain(map(filteredSignal, SIGNAL_THRESHOLD, 205, 90, 180), 90, 180);    // Map signal to a rotation speed
-                Serial.print("Rotation Speed = ");
-                Serial.println(rotation);
+                // Serial.print("Rotation Speed = ");
+                // Serial.println(rotation);
                 MOTORS[i].write(rotation);
                 totalRotation[i] += rotation;    
             }
 
             else {
                 // Set to turn state
-                Serial.println("Turn State");
+                // Serial.println("Turn State");
                 isOverdrawn[i] = false;
                 float rotation = map(filteredSignal, SIGNAL_THRESHOLD, 205, 90, 180);    // Map signal to a rotation speed
-                Serial.print("Rotation Speed = ");
-                Serial.println(rotation);
+                // Serial.print("Rotation Speed = ");
+                // Serial.println(rotation);
                 MOTORS[i].write(rotation);
                 totalRotation[i] += rotation;    
             }
